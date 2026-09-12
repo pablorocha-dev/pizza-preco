9199cc9e8fd966bf71c461a53ee2c27d7be21667
Pablo Rocha <rocha.pablost@gmail.com>
2026-09-12 13:31:30 -0300

    estrutura inicial spring boot

---
+++ src/main/java/br/edu/ifpa/pizzapreco/PizzaPrecoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpa.pizzapreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPrecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaPrecoApplication.class, args);
+	}
+
+}
