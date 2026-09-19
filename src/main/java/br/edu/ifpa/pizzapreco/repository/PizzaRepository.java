6a8bc4f1babcc4c422f3597f5bfe2fc2e11dfa38
Pablo Rocha <rocha.pablost@gmail.com>
2026-09-19 18:03:39 -0300

    add verificar nome e tamanho da pizza no repository

--- src/main/java/br/edu/ifpa/pizzapreco/repository/PizzaRepository.java
+++ src/main/java/br/edu/ifpa/pizzapreco/repository/PizzaRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
 
+    boolean existsByNomeAndTamanho(String nome, String tamanho);
+
 }
