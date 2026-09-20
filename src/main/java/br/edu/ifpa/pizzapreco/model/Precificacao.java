c82d0590aa83d1ba98fee31f38e49c76538fb053
Pablo Rocha <rocha.pablost@gmail.com>
2026-09-20 02:20:43 -0300

    removi o metodo calcularMargem() de precificacao

--- src/main/java/br/edu/ifpa/pizzapreco/model/Precificacao.java
+++ src/main/java/br/edu/ifpa/pizzapreco/model/Precificacao.java
@@ -1,7 +1,6 @@
 package br.edu.ifpa.pizzapreco.model;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class Precificacao {
 
@@ -42,21 +41,5 @@ public class Precificacao {
 
     public void setMargemLucro(BigDecimal margemLucro) {
         this.margemLucro = margemLucro;
-    }
-
-    public BigDecimal calcularMargem() {
-        if (custoTotal == null || lucro == null) {
-            return BigDecimal.ZERO;
-        }
-
-        BigDecimal precoVenda = lucro.add(custoTotal);
-
-        if (precoVenda.compareTo(BigDecimal.ZERO) == 0) {
-            return BigDecimal.ZERO;
-        }
-
-        return lucro
-                .divide(precoVenda, 4, RoundingMode.HALF_UP)
-                .multiply(BigDecimal.valueOf(100));
     }
 }
